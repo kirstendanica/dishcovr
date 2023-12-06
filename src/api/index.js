f204db3625620e31b65401eac57d6b91b05a7974
kirstendanica <kirdanic@gmail.com>
2023-12-05 20:39:35 -0500

    Set up Express server to provide static files via React app, and track dishcovr-app React app

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,19 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Serve the React app on the root route
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '..', 'dishcovr-app', 'build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dishcovr-app', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
